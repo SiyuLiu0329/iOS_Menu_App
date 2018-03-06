3c6c2e88e275a017ba4a10b821d928c2535e6b57
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-03-06 10:55:06 +1000

    view -> label

---
+++ OrderTracker/Controllers/Details/Summary/submit/paymentOption.swift
@@ -0,0 +1,59 @@
+//
+//  paymentOption.swift
+//  OrderTracker
+//
+//  Created by macOS on 6/3/18.
+//  Copyright © 2018 macOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol PaymentOptionDelegate: class {
+    func paymentOption(withIdSelected id: Int)
+}
+
+class PaymentOption: UILabel {
+    var id: Int?
+    var themeColour: UIColor?
+    var isSelected = false {
+        willSet {
+            if newValue == true {
+                UIView.animate(withDuration: 0.2) {
+                    self.backgroundColor = self.themeColour!
+                    self.textColor = .white
+                    self.frame = self.superview!.bounds
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.backgroundColor = .white
+                    self.textColor = self.themeColour
+                }
+            }
+        }
+    }
+    weak var delegate: PaymentOptionDelegate?
+    
+    func configureLabel(paymentOption text: String, labelID id: Int, themeColour colour: UIColor) {
+        textAlignment = .center
+        textColor = colour
+        self.themeColour = colour
+        self.id = id
+        self.text = text
+        font = UIFont.systemFont(ofSize: 30, weight: .light)
+        configureGesture()
+    }
+    
+    private func configureGesture() {
+        isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapped))
+        addGestureRecognizer(tap)
+        
+    }
+    
+    @objc private func tapped() {
+        if delegate != nil && id != nil {
+            delegate!.paymentOption(withIdSelected: id!)
+        }
+    }
+}
