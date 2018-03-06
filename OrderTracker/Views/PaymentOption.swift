11060bc6bd288a5633a6f23d1f154e635da9c6be
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-03-07 01:33:45 +1000

    payment options

---
+++ OrderTracker/Views/PaymentOption.swift
@@ -0,0 +1,44 @@
+import Foundation
+import UIKit
+
+protocol PaymentOptionDelegate: class {
+    func selectOption(withId id: Int)
+}
+
+class PaymentOption: UIView {
+    var optionLabel: UILabel?
+    var themeColour: UIColor?
+    var intialFrame: CGRect?
+    var originalCenterX: CGFloat?
+    var id: Int?
+    var isSelected = false 
+    weak var delegate: PaymentOptionDelegate?
+    func configureOption(frame rect: CGRect, themColour colour: UIColor, optionText text: String, viewId number: Int) {
+        self.frame = rect
+        self.themeColour = colour
+        self.intialFrame = rect
+        layer.borderWidth = 0.5
+        layer.borderColor = themeColour!.cgColor
+        clipsToBounds = true
+        
+        optionLabel = UILabel()
+        addSubview(optionLabel!)
+        optionLabel!.textAlignment = .center
+        optionLabel!.text = text
+        optionLabel!.frame = bounds
+
+        optionLabel!.font = UIFont.systemFont(ofSize: 25, weight: .light)
+        optionLabel!.textColor = themeColour!
+        id = number
+        originalCenterX = optionLabel!.center.x
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        addGestureRecognizer(tap)
+    }
+    
+    @objc private func tapped() {
+        if id != nil && delegate != nil {
+            delegate!.selectOption(withId: id!)
+        }
+    }
+}
