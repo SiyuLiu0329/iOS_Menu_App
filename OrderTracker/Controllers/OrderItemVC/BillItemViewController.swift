bb905a4d8d41f1952733331471c1f37acaea828e
David Liu <siyuliu@192-168-1-6.tpgi.com.au>
2018-03-31 12:18:40 +1000

    use collection view instead of separate controllers

--- OrderTracker/Controllers/OrderItemVC/BillItemViewController.swift
+++ OrderTracker/Controllers/OrderItemVC/BillItemViewController.swift
@@ -9,41 +9,29 @@
 import UIKit
 
 class BillItemViewController: UIViewController {
-    var views: [UIView]!
     var itemsToBill: [MenuItem]!
     var totalPrice: Double!
     var numberOfItems: Int!
-    @IBOutlet weak var bottomBar: UIToolbar!
     @IBOutlet weak var totalPriceLabel: UILabel!
     @IBOutlet weak var numberOfItemsLabel: UILabel!
+    @IBOutlet weak var toolbar: UIToolbar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         totalPriceLabel.text = Scheme.Util.twoDecimalPriceText(totalPrice!)
         numberOfItemsLabel.text =  "\(numberOfItems!) items"
-        views = []
-        views.append(createBillAllView())
-        views.append(SplitBillViewController().view)
-        for view in views {
-            containerView.addSubview(view)
-        }
-        // bring the first view to front ... (to fix switching problems)
-        containerView.bringSubview(toFront: views[0])
+        self.toolbar.setBackgroundImage(UIImage(),
+                                        forToolbarPosition: .any,
+                                        barMetrics: .default)
+        self.toolbar.setShadowImage(UIImage(), forToolbarPosition: .any)
     }
     
-    func createBillAllView() -> UIView {
-        let billAllViewVC = BillAllViewController()
-        billAllViewVC.view.frame = containerView.bounds
-        return billAllViewVC.view
-    }
-    
-    @IBOutlet weak var containerView: UIView!
     
     @IBAction func dismissAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func segmentedControlAction(_ sender: UISegmentedControl) {
-        containerView.bringSubview(toFront: views[sender.selectedSegmentIndex])
+        print(sender.selectedSegmentIndex)
     }
 }
---
+++ OrderTracker/VIews/SplitBillCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SplitBillCollectionViewCell.swift
+//  OrderTracker
+//
+//  Created by Siyu Liu on 31/3/18.
+//  Copyright © 2018 macOS. All rights reserved.
+//
+
+import UIKit
+
+class SplitBillCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
