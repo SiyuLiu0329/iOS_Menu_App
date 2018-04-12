d8313c079b7f5b9e491c5dfb99add437dd465634
David Liu <siyuliu@192-168-1-4.tpgi.com.au>
2018-04-12 16:37:15 +1000

    custom text field

---
+++ OrderTracker/Controllers/NetworkingVCs/ClientItemCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ClientItemCollectionViewCell.swift
+//  OrderTracker
+//
+//  Created by Siyu Liu on 4/4/18.
+//  Copyright © 2018 macOS. All rights reserved.
+//
+
+import UIKit
+
+class ClientItemCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- OrderTracker/VIews/ItemEditor/EditorItemTableViewHeaderCell.swift
+++ OrderTracker/VIews/ItemEditor/EditorItemTableViewHeaderCell.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class EditorItemTableViewHeaderCell: UITableViewCell {
 
-    @IBOutlet weak var nameField: CustomTextField!
     @IBOutlet weak var itemImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.cornerRadius = 10
         itemImageView.layer.cornerRadius = 5
         itemImageView.clipsToBounds = true
-        nameField.placeholder = "Item name"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
