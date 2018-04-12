0fa1a01eef71375a9a42e743ca6176e471b188c5
David Liu <siyuliu@siyus-macbook-pro.local>
2018-04-13 00:32:43 +1000

    data source file added

--- OrderTracker/Controllers/DetailVC/ItemEditorViewController.swift
+++ OrderTracker/Controllers/DetailVC/ItemEditorViewController.swift
@@ -15,14 +15,16 @@ class ItemEditorViewController: UIViewController {
     var menuModel: MenuModel!
     var itemIndex: Int?
     var itemEditorModel = ItemEditorModel()
+    private var dataSource: ItemEditorViewControllerDataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
         let cell = UINib(nibName: "ItemEditorInputCell", bundle: Bundle.main)
         tableView.register(cell, forCellReuseIdentifier: "cell")
+        dataSource = ItemEditorViewControllerDataSource(editorModel: itemEditorModel, itemIndex: itemIndex!)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "opCell")
-        tableView.dataSource = self
+        tableView.dataSource = dataSource
         tableView.delegate = self
         tableView.separatorColor = .clear
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.onDoneButtonPressed(_:)))
@@ -45,103 +47,8 @@ class ItemEditorViewController: UIViewController {
     }
 }
 
-extension ItemEditorViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else if section == 1 {
-            return 1
-        } else {
-            return itemEditorModel.options.count + 1
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let header = Bundle.main.loadNibNamed("EditorItemTableViewHeaderCell", owner: self, options: nil)!.first as! EditorItemTableViewHeaderCell
-            header.selectionStyle = .none
-            if let image = itemEditorModel.image {
-                header.itemImageView.image = image
-            } else {
-                // new item, use place holder image
-                header.itemImageView.image = #imageLiteral(resourceName: "placeholder")
-            }
-            header.configure(itemName: itemEditorModel.name, itemNumber: itemIndex! + 1, itemPrice: itemEditorModel.price)
-            return header
-        } else if indexPath.section == 1 {
-            // section for colours
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ItemEditorInputCell
-            cell.selectionStyle = .none
-            cell.clipsToBounds = true
-            cell.accessoryType = .disclosureIndicator
-            cell.layer.cornerRadius = 10
-            if let colour = itemEditorModel.colour {
-                cell.setColour(colour)
-            } else {
-                cell.setColour(UIColor(red: 1, green: 0, blue: 0, alpha: 1))
-            }
-            
-            return cell
-        } else {
-            // section for options
-            let cell = tableView.dequeueReusableCell(withIdentifier: "opCell", for: indexPath)
-            cell.selectionStyle = .none
-            cell.clipsToBounds = true
-            if indexPath.row == 0 {
-                cell.textLabel?.text = "Add Option"
-                cell.accessoryType = .disclosureIndicator
-                cell.textLabel?.textColor = Scheme.editorThemeColour
-            } else {
-                cell.textLabel?.text = itemEditorModel.options[indexPath.row - 1].description
-                cell.accessoryType = .none
-                cell.textLabel?.tintColor = .black
-            }
-            
-            if itemEditorModel.options.count == 0 {
-                // if this cell is the only cell, all four corners are rounded
-                cell.layer.cornerRadius = 10
-                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-                return cell
-            }
-            
-            if indexPath.row == 0 {
-                cell.layer.cornerRadius = 10
-                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-            } else if indexPath.row == itemEditorModel.options.count {
-                cell.layer.cornerRadius = 10
-                cell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
-            } else {
-                cell.layer.cornerRadius = 0
-            }
-            return cell
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 {
-            let sender = tableView.cellForRow(at: indexPath)!
-            let colorSelectionController = EFColorSelectionViewController()
-            let navCtrl = UINavigationController(rootViewController: colorSelectionController)
-            navCtrl.navigationBar.backgroundColor = UIColor.white
-            navCtrl.navigationBar.isTranslucent = false
-            navCtrl.modalPresentationStyle = UIModalPresentationStyle.popover
-            navCtrl.popoverPresentationController?.sourceView = sender
-            navCtrl.popoverPresentationController?.sourceRect = sender.bounds
-            
-            colorSelectionController.delegate = self
-            colorSelectionController.color = itemEditorModel.colour ?? UIColor.red
-            
-            self.present(navCtrl, animated: true, completion: nil)
-            
-        }
-        
-    }
-    
+extension ItemEditorViewController: UITableViewDelegate {
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.section == 0 {
             return 220
@@ -167,7 +74,26 @@ extension ItemEditorViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         return UIView()
     }
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 {
+            let sender = tableView.cellForRow(at: indexPath)!
+            let colorSelectionController = EFColorSelectionViewController()
+            let navCtrl = UINavigationController(rootViewController: colorSelectionController)
+            navCtrl.navigationBar.backgroundColor = UIColor.white
+            navCtrl.navigationBar.isTranslucent = false
+            navCtrl.modalPresentationStyle = UIModalPresentationStyle.popover
+            navCtrl.popoverPresentationController?.sourceView = sender
+            navCtrl.popoverPresentationController?.sourceRect = sender.bounds
+            
+            colorSelectionController.delegate = self
+            colorSelectionController.color = itemEditorModel.colour ?? UIColor.red
+            
+            self.present(navCtrl, animated: true, completion: nil)
+            
+        }
+        
+    }
 }
 
 extension ItemEditorViewController: EFColorSelectionViewControllerDelegate {
---
+++ OrderTracker/Controllers/DetailVC/ItemEditorViewControllerDataSource.swift
@@ -0,0 +1,96 @@
+//
+//  ItemEditorViewControllerDataSource.swift
+//  OrderTracker
+//
+//  Created by Siyu Liu on 13/4/18.
+//  Copyright © 2018 macOS. All rights reserved.
+//
+
+import UIKit
+
+class ItemEditorViewControllerDataSource: NSObject, UITableViewDataSource {
+    var itemEditorModel: ItemEditorModel
+    var itemIndex: Int
+    init(editorModel model: ItemEditorModel, itemIndex index: Int) {
+        itemEditorModel = model
+        itemIndex = index
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else if section == 1 {
+            return 1
+        } else {
+            return itemEditorModel.options.count + 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let header = Bundle.main.loadNibNamed("EditorItemTableViewHeaderCell", owner: self, options: nil)!.first as! EditorItemTableViewHeaderCell
+            header.selectionStyle = .none
+            if let image = itemEditorModel.image {
+                header.itemImageView.image = image
+            } else {
+                // new item, use place holder image
+                header.itemImageView.image = #imageLiteral(resourceName: "placeholder")
+            }
+            header.configure(itemName: itemEditorModel.name, itemNumber: itemIndex + 1, itemPrice: itemEditorModel.price)
+            return header
+        } else if indexPath.section == 1 {
+            // section for colours
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ItemEditorInputCell
+            cell.selectionStyle = .none
+            cell.clipsToBounds = true
+            cell.accessoryType = .disclosureIndicator
+            cell.layer.cornerRadius = 10
+            if let colour = itemEditorModel.colour {
+                cell.setColour(colour)
+            } else {
+                cell.setColour(UIColor(red: 1, green: 0, blue: 0, alpha: 1))
+            }
+            
+            return cell
+        } else {
+            // section for options
+            let cell = tableView.dequeueReusableCell(withIdentifier: "opCell", for: indexPath)
+            cell.selectionStyle = .none
+            cell.clipsToBounds = true
+            if indexPath.row == 0 {
+                cell.textLabel?.text = "Add Option"
+                cell.accessoryType = .disclosureIndicator
+                cell.textLabel?.textColor = Scheme.editorThemeColour
+            } else {
+                cell.textLabel?.text = itemEditorModel.options[indexPath.row - 1].description
+                cell.accessoryType = .none
+                cell.textLabel?.tintColor = .black
+            }
+            
+            if itemEditorModel.options.count == 0 {
+                // if this cell is the only cell, all four corners are rounded
+                cell.layer.cornerRadius = 10
+                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+                return cell
+            }
+            
+            if indexPath.row == 0 {
+                cell.layer.cornerRadius = 10
+                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            } else if indexPath.row == itemEditorModel.options.count {
+                cell.layer.cornerRadius = 10
+                cell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            } else {
+                cell.layer.cornerRadius = 0
+            }
+            return cell
+        }
+    }
+    
+
+    
+}
