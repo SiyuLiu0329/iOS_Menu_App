6266d99f7113443b7d6b84336af59f32445b2c08
David Liu <siyuliu@192-168-1-4.tpgi.com.au>
2018-04-14 21:00:10 +1000

    saving and loading menus from file

--- OrderTracker/Models/MenuModel.swift
+++ OrderTracker/Models/MenuModel.swift
@@ -16,6 +16,41 @@ class MenuModel {
     var menuItems: [MenuItem] = []
     var menuName: String
     
+    private func initDir() {
+        do {
+            try FileManager.default.createDirectory(at: savePath, withIntermediateDirectories: true, attributes: nil)
+        } catch let e {
+            print(e)
+        }
+    }
+    
+    private func saveItem(item: MenuItem) {
+        let url = savePath.appendingPathComponent(item.typeHash! + ".json")
+        do {
+            let data = try JSONEncoder().encode(item)
+            try data.write(to: url)
+        } catch let e {
+            print(e)
+        }
+    }
+    
+    private func loadItems() {
+        do {
+            let decoder = JSONDecoder()
+            let files = try FileManager.default.contentsOfDirectory(at: savePath, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+            for file in files {
+                let json = try Data.init(contentsOf: file)
+                let item = try decoder.decode(MenuItem.self, from: json)
+                menuItems.append(item)
+                
+            }
+            menuItems = menuItems.sorted(by: { $0.number < $1.number })
+
+        } catch let error {
+            print(error)
+        }
+    }
+    
     func toggleOptionValue(at optionIndex: Int, inPendingItem index: Int) {
         menuItems[index].toggleSelectetState(ofOption: optionIndex)
     }
@@ -30,6 +65,8 @@ class MenuModel {
     
     init(menuName name: String) {
         self.menuName = name
+        initDir()
+        loadItems()
     }
     
     func addItemToMenu(item: MenuItem) {
@@ -38,7 +75,9 @@ class MenuModel {
         } else {
             menuItems.append(item)
         }
+        saveItem(item: item)
     }
+
 
 }
 
